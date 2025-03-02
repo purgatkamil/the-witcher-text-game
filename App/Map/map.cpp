28e11a4f343b634d8e649e39e832d7fe342e50c4
Kamil Purgat <kpurgat.zsp9@gmail.com>
2025-03-02 18:58:39 +0100

    Add and connect new locations

--- App/Map/map.cpp
+++ App/Map/map.cpp
@@ -2,20 +2,46 @@
 
 void createWorld(GameWorld& world, Location*& startingLocation) {
     Location* KaerMorhen = new Location(KAER_MORHEN, "KaerMorhen", world);
-        Location* KaerMorhen_Courtyard_Lower = new Location(KAER_MORHEN_COURTYARD_LOWER, "Lower Courtyard", world);
-        Location* KaerMorhen_Courtyard_Middle = new Location(KAER_MORHEN_COURTYARD_MIDDLE, "Middle Courtyard", world);
-        Location* KaerMorhen_Courtyard_Upper = new Location(KAER_MORHEN_COURTYARD_UPPER, "Upper Courtyard", world);
+        Location* KaerMorhen_LowerCourtyard = new Location(KAER_MORHEN_COURTYARD_LOWER, "Lower Courtyard", world);
+            Location* KaerMorhen_MiddleCourtyard = new Location(KAER_MORHEN_COURTYARD_MIDDLE, "Middle Courtyard", world);
+                Location* KaerMorhen_UpperCourtyard = new Location(KAER_MORHEN_COURTYARD_UPPER, "Upper Courtyard", world);
+                    Location* KaerMorhen_Fortress = new Location(KAER_MORHEN_FORTRESS, "Fortress", world);
+                        Location* KaerMorhen_Fortress_Laboratory = new Location(KAER_MORHEN_LABORATORY, "Laboratory", world);
+                            Location* KaerMorhen_Fortress_Laboratory_WhisperingStoneCircle = new Location(KAER_MORHEN_WHISPERING_STONE_CIRCLE, "Whispering Stone Circle Hall", world);
+                        Location* KaerMorhen_Fortress_Kitchen = new Location(KAER_MORHEN_KITCHEN, "Kitchen", world);
+                            Location* KaerMorhen_Fortress_WestWing = new Location(KAER_MORHEN_WEST_WING, "West wing", world);
+                                Location* KaerMorhen_Fortress_WestWing_Library = new Location(KAER_MORHEN_LIBRARY, "Library", world);
+                                Location* KaerMorhen_Fortress_WestWing_Armory = new Location(KAER_MORHEN_ARMORY, "Armory", world);
+                                Location* KaerMorhen_Fortress_WestWing_EveningRoom = new Location(KAER_MORHEN_EVENING_ROOM, "Evening room", world);
+                                Location* KaerMorhen_Fortress_WestWing_TrissRoom = new Location(KAER_MORHEN_TRISS_ROOM, "Triss' room", world);
+                        Location* KaerMorhen_Fortress_GreatHall = new Location(KAER_MORHEN_GREAT_HALL, "Great hall", world);
 
-        KaerMorhen->addChildLocation(KaerMorhen_Courtyard_Lower);
-        KaerMorhen->addChildLocation(KaerMorhen_Courtyard_Middle);
-        KaerMorhen->addChildLocation(KaerMorhen_Courtyard_Upper);
+        KaerMorhen_LowerCourtyard->addExit("middle", KaerMorhen_MiddleCourtyard);
+            KaerMorhen_MiddleCourtyard->addExit("lower", KaerMorhen_LowerCourtyard);
+            KaerMorhen_MiddleCourtyard->addExit("upper", KaerMorhen_UpperCourtyard);
+                KaerMorhen_UpperCourtyard->addExit("middle", KaerMorhen_MiddleCourtyard);
+                KaerMorhen_UpperCourtyard->addExit("fortress", KaerMorhen_Fortress);
+                    KaerMorhen_Fortress->addExit("upper", KaerMorhen_UpperCourtyard);
+                    KaerMorhen_Fortress->addExit("laboratory", KaerMorhen_Fortress_Laboratory);
+                        KaerMorhen_Fortress_Laboratory->addExit("fortress", KaerMorhen_Fortress);
+                        KaerMorhen_Fortress_Laboratory->addExit("elementalstone", KaerMorhen_Fortress_Laboratory_WhisperingStoneCircle);
+                            KaerMorhen_Fortress_Laboratory_WhisperingStoneCircle->addExit("laboratory", KaerMorhen_Fortress_Laboratory);
+                    KaerMorhen_Fortress->addExit("kitchen", KaerMorhen_Fortress_Kitchen);
+                        KaerMorhen_Fortress_Kitchen->addExit("fortress", KaerMorhen_Fortress);
+                        KaerMorhen_Fortress_Kitchen->addExit("westwing", KaerMorhen_Fortress_WestWing);
+                            KaerMorhen_Fortress_WestWing->addExit("kitchen", KaerMorhen_Fortress_Kitchen);
+                                KaerMorhen_Fortress_WestWing->addExit("library", KaerMorhen_Fortress_WestWing_Library);
+                                    KaerMorhen_Fortress_WestWing_Library->addExit("westwing", KaerMorhen_Fortress_WestWing);
+                                KaerMorhen_Fortress_WestWing->addExit("armory", KaerMorhen_Fortress_WestWing_Armory);
+                                    KaerMorhen_Fortress_WestWing_Armory->addExit("westwing", KaerMorhen_Fortress_WestWing);
+                                KaerMorhen_Fortress_WestWing->addExit("eveningroom", KaerMorhen_Fortress_WestWing_EveningRoom);
+                                    KaerMorhen_Fortress_WestWing_EveningRoom->addExit("westwing", KaerMorhen_Fortress_WestWing);
+                                KaerMorhen_Fortress_WestWing->addExit("trissroom", KaerMorhen_Fortress_WestWing_TrissRoom);
+                                    KaerMorhen_Fortress_WestWing_TrissRoom->addExit("westwing", KaerMorhen_Fortress_WestWing);
+                    KaerMorhen_Fortress->addExit("greathall", KaerMorhen_Fortress_GreatHall);
+                    KaerMorhen_Fortress_GreatHall->addExit("fortress", KaerMorhen_Fortress);
 
-        KaerMorhen_Courtyard_Lower->addExit("middle", KaerMorhen_Courtyard_Middle);
-        KaerMorhen_Courtyard_Middle->addExit("lower", KaerMorhen_Courtyard_Lower);
-        KaerMorhen_Courtyard_Middle->addExit("upper", KaerMorhen_Courtyard_Upper);
-        KaerMorhen_Courtyard_Upper->addExit("middle", KaerMorhen_Courtyard_Middle);
-
-    startingLocation = KaerMorhen_Courtyard_Lower;
+    startingLocation = KaerMorhen_LowerCourtyard;
 }
 
 void displayLocation(Location* current) {
@@ -26,13 +52,6 @@ void displayLocation(Location* current) {
         std::cout << "\nYou can move within this area:\n";
         for (const auto& [dir, loc] : current->exits) {
             std::cout << "  - " << dir << " -> " << loc->name << "\n";
-        }
-    }
-
-    if (!current->childLocations.empty()) {
-        std::cout << "\nThis place contains:\n";
-        for (const auto& [name, loc] : current->childLocations) {
-            std::cout << "  - " << name << "\n";
         }
     }
 
