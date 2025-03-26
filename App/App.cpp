6dbb644c4ad0c5c9b962b7d3462ad38a329e5053
Kamil Purgat <kpurgat.zsp9@gmail.com>
2025-03-26 07:06:37 +0100

    ADd template for test run

--- App/App.cpp
+++ App/App.cpp
@@ -2,7 +2,17 @@
 #include "Character/character.hpp"
 #include "Fight/fight.hpp"
 
-int main() {
+void runAutoTest(void)
+{
+
+}
+
+int main(int argc, char* argv[]) {
+    if (argc > 1 && std::string(argv[1]) == "--test-mode") {
+        runAutoTest(); 
+        return 0;
+    }
+
     GameWorld world;
     Geralt Geralt;
 
