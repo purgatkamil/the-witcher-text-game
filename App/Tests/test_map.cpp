25fff869807251d047f4e98ed638b6f3ba9a20eb
Kamil Purgat <kpurgat.zsp9@gmail.com>
2025-03-26 16:22:53 +0100

    Adjust test to use new type of pointers

--- App/Tests/test_map.cpp
+++ App/Tests/test_map.cpp
@@ -10,68 +10,67 @@ public:
 
 TEST(LocationTest, ConstructorTest) {
     GameWorld world;
-    Location loc(KAER_MORHEN, "Kaer Morhen", "kaermorhen", world);
+    std::shared_ptr<Location> loc = std::make_shared<Location>(KAER_MORHEN, "Kaer Morhen", "kaermorhen");
 
-    EXPECT_EQ(loc.id, KAER_MORHEN);
-    EXPECT_EQ(loc.name, "Kaer Morhen");
-    EXPECT_EQ(loc.label, "kaermorhen");
-    EXPECT_EQ(world["Kaer Morhen"], &loc);
+    EXPECT_EQ(loc->id, KAER_MORHEN);
+    EXPECT_EQ(loc->name, "Kaer Morhen");
+    EXPECT_EQ(loc->label, "kaermorhen");
 }
 
 TEST(LocationTest, GetDescriptionTest) {
     GameWorld world;
-    Location loc(KAER_MORHEN, "Kaer Morhen", "kaermorhen", world);
+    std::shared_ptr<Location> loc = std::make_shared<Location>(KAER_MORHEN, "Kaer Morhen", "kaermorhen");
 
-    EXPECT_EQ(MockDescriptions::getDescription(loc.id), 
+    EXPECT_EQ(MockDescriptions::getDescription(loc->id), 
               "Mock description for location ID " + std::to_string(KAER_MORHEN));
 }
 
 TEST(LocationTest, AddExitTest) {
     GameWorld world;
-    Location locA(KAER_MORHEN, "Kaer Morhen", "kaermorhen", world);
-    Location locB(KAER_MORHEN_COURTYARD_LOWER, "Lower Courtyard", "lower", world);
+    std::shared_ptr<Location> locA = std::make_shared<Location>(KAER_MORHEN, "Kaer Morhen", "kaermorhen");
+    std::shared_ptr<Location> locB = std::make_shared<Location>(KAER_MORHEN_COURTYARD_LOWER, "Lower Courtyard", "lower");
 
-    locA.addExit(&locB);
-    EXPECT_EQ(locA.exits["lower"], &locB);
+    locA->addExit(locB);
+    EXPECT_EQ(locA->exits["lower"], locB);
 }
 
 TEST(LocationTest, AddGlobalExitTest) {
     GameWorld world;
-    Location locA(KAER_MORHEN, "Kaer Morhen", "kaermorhen", world);
-    Location locB(KAER_MORHEN_FORTRESS, "Fortress", "fortress", world);
+    std::shared_ptr<Location> locA = std::make_shared<Location>(KAER_MORHEN, "Kaer Morhen", "kaermorhen");
+    std::shared_ptr<Location> locB = std::make_shared<Location>(KAER_MORHEN_FORTRESS, "Fortress", "fortress");
 
-    locA.addGlobalExit(&locB);
-    EXPECT_EQ(locA.globalExits["fortress"], &locB);
+    locA->addGlobalExit(locB);
+    EXPECT_EQ(locA->globalExits["fortress"], locB);
 }
 
 TEST(LocationTest, ConnectTest) {
     GameWorld world;
-    Location locA(KAER_MORHEN, "Kaer Morhen", "kaermorhen", world);
-    Location locB(KAER_MORHEN_COURTYARD_LOWER, "Lower Courtyard", "lower", world);
+    std::shared_ptr<Location> locA = std::make_shared<Location>(KAER_MORHEN, "Kaer Morhen", "kaermorhen");
+    std::shared_ptr<Location> locB = std::make_shared<Location>(KAER_MORHEN_COURTYARD_LOWER, "Lower Courtyard", "lower");
 
-    locA.connect(&locB);
+    locA->connect(locB);
 
-    EXPECT_EQ(locA.exits["lower"], &locB);
-    EXPECT_EQ(locB.exits["kaermorhen"], &locA);
+    EXPECT_EQ(locA->exits["lower"], locB);
+    EXPECT_EQ(locB->exits["kaermorhen"], locA);
 }
 
 TEST(WorldTest, CreateWorldTest) {
     GameWorld world;
-    Location* startingLocation = nullptr;
-    
+    std::shared_ptr<Location> startingLocation;
+
     createWorld(world, startingLocation);
 
-    EXPECT_NE(startingLocation, nullptr);
+    ASSERT_NE(startingLocation, nullptr);
     EXPECT_EQ(startingLocation->name, "Lower Courtyard");
     EXPECT_EQ(startingLocation->label, "lower");
 }
 
 TEST(WorldTest, DisplayLocationTest) {
     GameWorld world;
-    Location loc(KAER_MORHEN, "Kaer Morhen", "kaermorhen", world);
+    std::shared_ptr<Location> loc = std::make_shared<Location>(KAER_MORHEN, "Kaer Morhen", "kaermorhen");
 
     testing::internal::CaptureStdout();
-    displayLocation(&loc);
+    displayLocation(loc);
     std::string output = testing::internal::GetCapturedStdout();
 
     EXPECT_NE(output.find("You are in: Kaer Morhen"), std::string::npos);
